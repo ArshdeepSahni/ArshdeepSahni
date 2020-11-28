b705bed787f52022b7d67c88236fc6e4f017f8ff
Arshdeep2001 <johndoe@example.com>
2020-11-29 01:36:52 +0530

    Personal Portfolio

---
+++ src/containers/education/Education.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Education.css';
+import emoji from "react-easy-emoji";
+import EducationCard from '../../components/educationCard/EducationCard';
+import { educationInfo } from '../../portfolio';
+
+export default function Education() {
+  return (
+    <div className="education-section" id="education">
+      <h1 className="education-heading">Education <span className="heart-emoji">📚</span></h1>
+      <div className="education-card-container">
+        {educationInfo.schools.map((school) => (
+          <EducationCard school={school} />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/containers/workExperience/WorkExperience.js
@@ -0,0 +1,42 @@
+import React, { useContext } from "react";
+import "./WorkExperience.css";
+import emoji from "react-easy-emoji";
+import ExperienceCard from "../../components/experienceCard/ExperienceCard";
+import { workExperiences } from "../../portfolio";
+import { Fade } from "react-reveal";
+import StyleContext from "../../contexts/StyleContext";
+
+export default function WorkExperience() {
+    const { isDark } = useContext(StyleContext)
+    if (workExperiences.viewExperiences) {
+        return (
+            <div id="experience">
+                <Fade bottom duration={1000} distance="20px">
+                    <div className="experience-container" id="workExperience">
+                        <div>
+                            <h1 className="experience-heading">Experiences <span className="heart-emoji">💼</span></h1>
+                            <div className="experience-cards-div">
+                                {workExperiences.experience.map((card) => {
+                                    return (
+                                        <ExperienceCard
+                                            isDark={isDark}
+                                            cardInfo={{
+                                                company: card.company,
+                                                desc: card.desc,
+                                                date: card.date,
+                                                companylogo: card.companylogo,
+                                                role: card.role,
+                                                descBullets: card.descBullets
+                                            }}
+                                        />
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                </Fade>
+            </div>
+        );
+    }
+    return null;
+}
